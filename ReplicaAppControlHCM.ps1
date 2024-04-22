6c9f185ea72dcc2f94970a099ed740cff9c86b99
Igor Rodrigues <75752692+igordriguess@users.noreply.github.com>
2024-04-22 15:23:47 -0300

    Update ReplicaAppControlHCM.ps1

--- ReplicaAppControlHCM.ps1
+++ ReplicaAppControlHCM.ps1
@@ -3,31 +3,31 @@ While ($true) {
 Clear-Host;
 Write-Host "Replicar aquivo 'appcontrol.ini' do HCM" -ForegroundColor Yellow;
     
-    $server = "OCMEGTSHCM01"
+    $server = "OCMEGTSHCM02"
     Test-Connection -Computername $server -BufferSize 16 -Count 1 -Quiet
 
-    $server2 = "OCMEGTSMETA04"
+    $server2 = "OCMEGTSHCM03"
     Test-Connection -Computername $server2 -BufferSize 16 -Count 1 -Quiet
 
-    $server3 = "OCMEGTSHCM02"
+    $server3 = "OCMEGTSHCM04"
     Test-Connection -Computername $server2 -BufferSize 16 -Count 1 -Quiet
 
-    $server4 = "OCMEGTSHCM03"
+    $server4 = "OCMEGTSHCM05"
     Test-Connection -Computername $server2 -BufferSize 16 -Count 1 -Quiet
 
     $copy = Read-Host "Digite (S) para replicar o AppControl do HCM"
 
     if($copy -eq "S"){
-        Copy-Item -Path "\\OCMEGTSMETA03\c$\Program Files (x86)\TSplus\UserDesktop\files\AppControl.ini" -Destination "\\$server\c$\Program Files (x86)\TSplus\UserDesktop\files\"
+        Copy-Item -Path "\\OCMEGTSHCM01\c$\Program Files (x86)\TSplus\UserDesktop\files\AppControl.ini" -Destination "\\$server\c$\Program Files (x86)\TSplus\UserDesktop\files\"
 
          }if($copy -eq "S"){
-        Copy-Item -Path "\\OCMEGTSMETA03\c$\Program Files (x86)\TSplus\UserDesktop\files\AppControl.ini" -Destination "\\$server2\c$\Program Files (x86)\TSplus\UserDesktop\files\"
+        Copy-Item -Path "\\OCMEGTSHCM01\c$\Program Files (x86)\TSplus\UserDesktop\files\AppControl.ini" -Destination "\\$server2\c$\Program Files (x86)\TSplus\UserDesktop\files\"
 
          }if($copy -eq "S"){
-        Copy-Item -Path "\\OCMEGTSMETA03\c$\Program Files (x86)\TSplus\UserDesktop\files\AppControl.ini" -Destination "\\$server3\c$\Program Files (x86)\TSplus\UserDesktop\files\"
+        Copy-Item -Path "\\OCMEGTSHCM01\c$\Program Files (x86)\TSplus\UserDesktop\files\AppControl.ini" -Destination "\\$server3\c$\Program Files (x86)\TSplus\UserDesktop\files\"
 
          }if($copy -eq "S"){
-        Copy-Item -Path "\\OCMEGTSMETA03\c$\Program Files (x86)\TSplus\UserDesktop\files\AppControl.ini" -Destination "\\$server4\c$\Program Files (x86)\TSplus\UserDesktop\files\"}
+        Copy-Item -Path "\\OCMEGTSHCM01\c$\Program Files (x86)\TSplus\UserDesktop\files\AppControl.ini" -Destination "\\$server4\c$\Program Files (x86)\TSplus\UserDesktop\files\"}
 
 	Write-Host "O Arquivo AppControl foi replicado com sucesso!!"
 
